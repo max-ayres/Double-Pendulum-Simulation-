2f347c70cbb3bd0272e0190e040f8ef7c6d26206
max-ayres <138659126+max-ayres@users.noreply.github.com>
2023-09-01 14:16:01 +0100

    Add files via upload

---
+++ singlependulum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <chrono>
+
+int main()
+{   
+    //constants
+    const double g = 9.81;
+    double m;
+    double L;
+    double theta0;
+    double theta;
+
+    double time0 = ;
+
+
+
+    //single pendulum equation of motion
+    double T = 2 * M_PI * sqrt(L/g);
+    double omega = (2 * M_PI)/T;
+    theta *= theta0 * cos(omega*time);
+
+
+
+    return 0;
+}
